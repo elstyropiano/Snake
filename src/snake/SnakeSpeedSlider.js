2370f1db4dd18c748cd97afad6956ab084dca735
Adamell000 <jasnypelnyfull@gmail.com>
2023-02-15 20:46:07 +0100

    added-color-change-to-slider

--- src/snake/SnakeSpeedSlider.js
+++ src/snake/SnakeSpeedSlider.js
@@ -1,12 +1,12 @@
-import { Typography, Slider, Box, styled, css } from "@mui/material"
-import { Stack } from "@mui/system"
+import { Typography, Slider, Box, styled, css } from "@mui/material";
+import { Stack } from "@mui/system";
 
 const Wrapper = styled("div")(
   css`
     display: flex;
     flex-direction: column;
   `
-)
+);
 
 const StyledTypography = styled(Typography)(
   ({ header }) =>
@@ -15,7 +15,7 @@ const StyledTypography = styled(Typography)(
       color: lightgray;
       font-size: ${header ? "28px" : "18px"};
     `
-)
+);
 
 const SliderDescriptionWrapper = styled("div")(
   css`
@@ -23,13 +23,17 @@ const SliderDescriptionWrapper = styled("div")(
     width: 100%;
     justify-content: space-between;
   `
-)
+);
 
-const StyledSlider = styled(Slider)(
-  css`
-    color: rgb(112, 99, 192);
-  `
-)
+const StyledSlider = styled(Slider)`
+  && {
+    color: ${({ value }) => `
+      rgb(${Math.round(255 * (100 - value)) / 100}, ${Math.round(
+      (255 * value) / 100
+    )}, 0)
+    `};
+  }
+`;
 
 const SnakeSpeedSlider = ({
   setSpeed,
@@ -38,18 +42,18 @@ const SnakeSpeedSlider = ({
   pointsMultiplier,
 }) => {
   const handleChange = (e) => {
-    const snakeSpeed = e.target.value
-    const newMultiplier = Math.ceil(snakeSpeed * 10) / 100
-    setSpeed(snakeSpeed)
-    setPointsMultiplier(newMultiplier)
-  }
+    const snakeSpeed = e.target.value;
+    const newMultiplier = Math.ceil(snakeSpeed * 10) / 100;
+    setSpeed(snakeSpeed);
+    setPointsMultiplier(newMultiplier);
+  };
 
   return (
     <Wrapper>
       <StyledTypography header gutterBottom>
         Please set the snake speed
       </StyledTypography>
-      <Stack spacing={2} direction='column' sx={{ mb: 1 }} alignItems='center'>
+      <Stack spacing={2} direction="column" sx={{ mb: 1 }} alignItems="center">
         <StyledSlider
           max={100}
           min={10}
@@ -66,7 +70,7 @@ const SnakeSpeedSlider = ({
         Points x {pointsMultiplier}
       </StyledTypography>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default SnakeSpeedSlider
+export default SnakeSpeedSlider;
